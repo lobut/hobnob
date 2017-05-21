a21b18382faad2c6b960ea6ca41389c72c64c83b
Robert Luong <robert.luong@gmail.com>
2017-05-21 23:03:34 +0100

    intialise repository

---
+++ src/components/App.js
@@ -0,0 +1,5 @@
+export const App = () => (
+  <h1>This is a header</h1>
+);
+
+export default App;
---
+++ src/server.js
@@ -0,0 +1,40 @@
+import path from 'path';
+import { Server } from 'http';
+import Express from 'express';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { StaticRouter as Router } from 'react-router-dom';
+import { App } from './components/App';
+
+const app = new Express();
+const server = new Server(app);
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(Express.static(path.join(__dirname, 'static')));
+
+app.get('*', (req, res) => {
+  let markup = '';
+  let status = 200;
+
+  const context = {};
+  markup = renderToString(
+    <div>
+      <h1>Robert Face</h1>
+    </div>
+  );
+
+  return res.status(status).render('index', { markup });
+});
+
+const port = process.env.PORT || 3000;
+const env = process.env.NODE_ENV || 'production';
+server.listen(port, (err) => {
+  if (err) {
+    return console.error(err);
+  }
+  return console.info(`
+    Server running on http://localhost:${port} [${env}]
+  `);
+})
